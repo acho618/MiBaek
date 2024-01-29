84626922593b1f7cd14c5a7a9856eb823ee0eb13
acho618 <a.cho618@gmail.com>
2024-01-28 21:45:51 -0500

    trying out login page and swift

--- MiBaek/ContentView.swift
+++ MiBaek/ContentView.swift
@@ -8,17 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongUsername = 0
+    @State private var wrongPassword = 0
+    @State private var showingLoginScreen = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        NavigationView{
+            ZStack {
+                Color.gray
+                    .ignoresSafeArea()
+                Circle()
+                    .scale(1.6)
+                    .foregroundColor(.white.opacity(3.0))
+                VStack{
+                    //Image(.miBaek)
+                      //  .resizable()
+                    Text("Login")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    TextField("Username", text: $username)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.green.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongUsername))
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.green.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongPassword))
+                    
+                    Button("Login"){
+                        // Authenticate the user
+                        authenticateUser(username: username, password: password)
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 50)
+                    .background(Color.green.opacity(0.8))
+                    .cornerRadius(10)
+                    
+                    //navigation link that will send the user to the right place once they login correctly
+                    NavigationLink(destination: Text("Welcome @\(username)"), isActive: $showingLoginScreen ){
+                        EmptyView()
+                    }
+                }
+            }
+        } .navigationBarHidden(true)
+    }
+    func authenticateUser(username: String, password: String){
+        if username.lowercased() == "aj2024" {
+            wrongUsername = 0
+            if password.lowercased() == "abc123"{
+                wrongPassword = 0
+                showingLoginScreen = true
+            } else {
+                wrongPassword = 2
+            }
+        } else{
+            wrongUsername = 2
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
